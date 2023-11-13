5027a12fc5262ebbe8993356637985914e63039c
Mustapha Musa <mustapha.musa@decagon.dev>
2023-11-13 17:34:20 +0100

    first commit

---
+++ backend-services/src/test/java/com/byteworks/dev/backendservices/BackendServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byteworks.dev.backendservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
