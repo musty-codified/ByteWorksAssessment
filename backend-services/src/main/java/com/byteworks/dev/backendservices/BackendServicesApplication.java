1166fc452a1710338734c1fe835a24981dc5b920
Mustapha Musa <mustapha.musa@decagon.dev>
2024-04-21 17:45:20 +0100

    Small update

--- backend-services/src/main/java/com/byteworks/dev/backendservices/BackendServicesApplication.java
+++ backend-services/src/main/java/com/byteworks/dev/backendservices/BackendServicesApplication.java
@@ -9,12 +9,9 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Arrays;
 
 
 @OpenAPIDefinition(
@@ -49,8 +46,7 @@ import java.util.Arrays;
 public class BackendServicesApplication {
 
 	public static void main(String[] args) {
-	ConfigurableApplicationContext context = SpringApplication.run(BackendServicesApplication.class, args);
-		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+	  SpringApplication.run(BackendServicesApplication.class, args);
 
 	}
 
